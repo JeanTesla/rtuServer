a70317663ba2bda2c3fcfeaa636f7e65f87550a7
Tesla <jean.tesla@outlook.com>
2021-10-26 19:39:57 -0300

    Correção variáveis de ambiente

--- src/serviceAPI.js
+++ src/serviceAPI.js
@@ -1,9 +1,11 @@
 const { default: axios } = require("axios")
+const BASE_URL = process.env.npm_package_config_BASE_URL
+const ENDPOINT = process.env.npm_package_config_ENDPOINT
 
 const berdiAPI = {
     //base_url: 'http://www.berdiservicos.com',
-    base_url: 'http://berdi.localhost/api',
-    level_endpoint: '/save_level',
+    base_url: BASE_URL,
+    level_endpoint: ENDPOINT,
     send: function(frameJson){
         return axios.post(this.base_url + this.level_endpoint,frameJson)
     }
